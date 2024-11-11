9306a5f833cd5d895c3f692991006760700bcac1
ObSp <150492054+ObSp@users.noreply.github.com>
2024-11-11 12:59:27 +0100

    s

--- JGamePackage/JGame/Classes/UI/UIText.java
+++ JGamePackage/JGame/Classes/UI/UIText.java
@@ -1,5 +1,43 @@
 package JGamePackage.JGame.Classes.UI;
 
-public class UIText {
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+
+import JGamePackage.JGame.Types.Constants.Constants;
+import JGamePackage.JGame.Types.PointObjects.Vector2;
+
+public class UIText extends UIBase{
+
     public String Text = "Text here";
+
+    public boolean TextScaled = false;
+    
+    public Color TextColor = Color.black;
+
+    public int HorizontalTextAlignment = Constants.HorizontalTextAlignment.Center;
+
+    public double VerticalTextAlignment = Constants.VerticalTextAlignment.Center;
+
+    public int FontSize = 10;
+    public String FontName = "Arial";
+    public int FontStyle = Font.PLAIN;
+
+    @Override
+    public void render(Graphics2D g) {
+        if (Text == null) return;
+
+        Vector2 renderSize = GetAbsoluteSize();
+        Vector2 renderPosition = GetAbsolutePosition();
+
+        if (!game.Camera.AreBoundsInCameraBounds(renderSize, renderPosition)) return;
+
+        Font font;
+        
+        if (TextScaled) {
+            font = new Font(this.FontName, this.FontStyle, (int) (renderSize.Y*1.3));
+        } else {
+            font = new Font(this.FontName, this.FontStyle, this.FontSize);
+        }
+    }
 }
--- JGamePackage/JGame/Types/Constants/Constants.java
+++ JGamePackage/JGame/Types/Constants/Constants.java
@@ -6,4 +6,16 @@ public final class Constants {
         public static final int X = 0;
         public static final int Y = 1;
     }
+
+    public final class HorizontalTextAlignment {
+        public static final int Left = 0;
+        public static final int Center = 1;
+        public static final int Right = 2;
+    }
+
+    public final class VerticalTextAlignment {
+        public static final int Top = 0;
+        public static final int Center = 1;
+        public static final int Bottom = 2;
+    }
 }
