d9dfde5433ee3c2405df94283009fbafb3bd66da
ObSp <150492054+ObSp@users.noreply.github.com>
2024-12-12 13:17:40 +0100

    d

---
+++ PaperAirplaneSimulation/ExperimentData.java
@@ -0,0 +1,5 @@
+package PaperAirplaneSimulation;
+
+public final class ExperimentData {
+    
+}
--- PaperAirplaneSimulation/Main.java
+++ PaperAirplaneSimulation/Main.java
@@ -8,6 +8,10 @@ import JGamePackage.JGame.Types.PointObjects.Vector2;
 
 public class Main {
     static JGame game = new JGame();
+
+    static Box2D plane;
+
+    static double c = 0;
     public static void main(String[] args) {
         game.Services.WindowService.BackgroundColor = new Color(76, 201, 254);
 
@@ -18,5 +22,17 @@ public class Main {
         b.FillColor = new Color(88,234,66);
         game.Camera.Position = game.Services.WindowService.GetScreenSize().divide(2);
         b.SetParent(game.WorldNode);
+
+        plane = new Box2D();
+        plane.Size = new Vector2(30, 5);
+        plane.Position = new Vector2(game.Services.WindowService.GetScreenWidth()-100, 150);
+        plane.Rotation = Math.toDegrees(45);
+        plane.SetParent(game.WorldNode);
+
+        game.Services.TimeService.OnTick.Connect(dt->calculateValues());
+    }
+
+    private static void calculateValues() {
+        c = plane.Rotation;
     }
 }
