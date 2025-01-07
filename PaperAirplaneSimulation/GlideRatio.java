14666fb5fe2a033a29bced929f11d91c61b4486c
ObSp <150492054+ObSp@users.noreply.github.com>
2025-01-07 21:16:43 +0100

    s

--- PaperAirplaneSimulation/GlideRatio.java
+++ PaperAirplaneSimulation/GlideRatio.java
@@ -3,24 +3,17 @@ package PaperAirplaneSimulation;
 import java.awt.Color;
 
 import JGamePackage.JGame.JGame;
-<<<<<<< HEAD
 import JGamePackage.JGame.Classes.UI.UIText;
 import JGamePackage.JGame.Classes.World.Box2D;
 import JGamePackage.JGame.Types.Constants.Constants;
 import JGamePackage.JGame.Types.PointObjects.UDim2;
 import JGamePackage.JGame.Types.PointObjects.Vector2;
-=======
-import JGamePackage.JGame.Classes.World.Box2D;
-import JGamePackage.JGame.Types.PointObjects.Vector2;
-import JGamePackage.lib.Signal.Signal.Connection;
->>>>>>> 3c5f3e8201f520f8b89fe48e4289b050c49e1954
 
 public class GlideRatio {
     static JGame game = new JGame();
 
     static Box2D plane;
 
-<<<<<<< HEAD
     static Vector2 velocity = new Vector2(0);
 
     static double drag = 0.0;
@@ -39,17 +32,6 @@ public class GlideRatio {
 
         System.out.println(game.Services.WindowService.GetScreenWidth());
 
-=======
-    static double velocity = 0.0;
-    static double wind = 0.5; //m/s
-
-    @SuppressWarnings("rawtypes")
-    static Connection tickConnection;
-
-    public static void main(String[] args) {
-        game.Services.WindowService.BackgroundColor = new Color(76, 201, 254);
-
->>>>>>> 3c5f3e8201f520f8b89fe48e4289b050c49e1954
         Box2D b = new Box2D();
         b.Size = new Vector2(game.Services.WindowService.GetScreenWidth(), 100);
         b.Pivot = new Vector2(0, 1);
@@ -60,7 +42,6 @@ public class GlideRatio {
 
         plane = new Box2D();
         plane.Size = new Vector2(30.4, 5);
-<<<<<<< HEAD
         plane.Position = new Vector2(game.Services.WindowService.GetScreenWidth()-100, game.Services.WindowService.GetScreenHeight()-b.Size.Y-465.8);
         plane.Rotation = Math.toDegrees(45);
         plane.SetParent(game.WorldNode);
@@ -68,32 +49,5 @@ public class GlideRatio {
         game.Services.TimeService.WaitSeconds(5);
 
         
-=======
-        plane.Pivot = Vector2.one;
-        plane.Position = new Vector2(0, game.Services.WindowService.GetScreenHeight() - b.Size.Y - ExperimentData.throwHeight * ExperimentData.meterInPixel);
-        plane.Rotation = Math.toDegrees(45);
-        plane.SetParent(game.WorldNode);
-
-        tickConnection = game.Services.TimeService.OnTick.Connect(dt->{
-            if (plane.Position.Y >= b.Position.Y - b.Size.Y) {
-                tickConnection.Disconnect();
-                System.out.println(plane.Position.X/ExperimentData.meterInPixel);
-                return;
-            }
-
-            double xInMeters = plane.Position.X/ExperimentData.meterInPixel; //m
-            xInMeters += .05;
-            
-            double yInMeters = -0.04*(xInMeters*xInMeters) + ExperimentData.throwHeight;
-
-            double yInPixels = yInMeters * ExperimentData.meterInPixel;
-            yInPixels = b.Position.Y - b.Size.Y - yInPixels;
-
-            double xInPixels = xInMeters * ExperimentData.meterInPixel;
-
-            plane.Position = new Vector2(xInPixels, yInPixels);
-            System.out.println(plane.Position);
-        });
->>>>>>> 3c5f3e8201f520f8b89fe48e4289b050c49e1954
     }
 }
