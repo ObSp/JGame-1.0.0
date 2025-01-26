1777e53d0f7ad9e33c16f128948bdb89c6d73185
ObSp <150492054+ObSp@users.noreply.github.com>
2025-01-26 13:27:48 +0100

    tried some stuff that didnt work

--- JGameStudio/HubStudioBridge/HubStudioBridge.java
+++ JGameStudio/HubStudioBridge/HubStudioBridge.java
@@ -1,20 +1,27 @@
 package JGameStudio.HubStudioBridge;
 
+import java.io.File;
 import java.io.IOException;
 
 public class HubStudioBridge {
     private static String username = System.getProperty("user.name");
-    private static String command = "powershell.exe cd 'c:\\Users\\%u\\Documents\\GitHub\\JGame-1.0.0' ; & 'C:\\Program Files\\Java\\jdk-21\\bin\\java.exe' '@C:\\Users\\%u\\AppData\\Local\\Temp\\cp_b1tap17rzruuy5jvwumf417xx.argfile' 'JGameStudio.Studio.Studio'";
-    //private static String command = "powershell.exe";
+    private static String command = "powershell.exe cd 'c:\\Users\\%u\\Documents\\GitHub\\JGame-1.0.0'; & 'C:\\Program Files\\Java\\jdk-21\\bin\\java.exe' '@C:\\Users\\%u\\AppData\\Local\\Temp\\cp_b1tap17rzruuy5jvwumf417xx.argfile' 'JGameStudio.Studio.Studio'";
+    //private static String command = "powershell.exe  'C:\\Program Files\\Java\\jdk-21\\bin\\java.exe' 'C:\\Users\\%u\\AppData\\Local\\Temp\\cp_b1tap17rzruuy5jvwumf417xx.argfile' 'JGameStudio.Studio.Studio'";
     
-	@SuppressWarnings("deprecation")
     public static void openProjectInStudio(String path) {
         try {
+            Runtime.getRuntime().exec(command.replaceAll("%u", username) + path);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        /*try {
             Runtime.getRuntime().exec(
                 command.replaceAll("%u", username) + path
             );
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 }
