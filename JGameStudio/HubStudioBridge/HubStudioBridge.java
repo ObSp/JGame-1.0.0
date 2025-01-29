80afc52e6148672c724ba2c706bdb0152f212b2f
ObSp <150492054+ObSp@users.noreply.github.com>
2025-01-29 18:57:17 +0100

    Update HubStudioBridge.java

--- JGameStudio/HubStudioBridge/HubStudioBridge.java
+++ JGameStudio/HubStudioBridge/HubStudioBridge.java
@@ -1,6 +1,5 @@
 package JGameStudio.HubStudioBridge;
 
-import java.io.File;
 import java.io.IOException;
 
 public class HubStudioBridge {
@@ -8,6 +7,7 @@ public class HubStudioBridge {
     private static String command = "powershell.exe cd 'c:\\Users\\%u\\Documents\\GitHub\\JGame-1.0.0'; & 'C:\\Program Files\\Java\\jdk-21\\bin\\java.exe' '@C:\\Users\\%u\\AppData\\Local\\Temp\\cp_b1tap17rzruuy5jvwumf417xx.argfile' 'JGameStudio.Studio.Studio'";
     //private static String command = "powershell.exe  'C:\\Program Files\\Java\\jdk-21\\bin\\java.exe' 'C:\\Users\\%u\\AppData\\Local\\Temp\\cp_b1tap17rzruuy5jvwumf417xx.argfile' 'JGameStudio.Studio.Studio'";
     
+    @SuppressWarnings("deprecation")
     public static void openProjectInStudio(String path) {
         try {
             Runtime.getRuntime().exec(command.replaceAll("%u", username) + path);
